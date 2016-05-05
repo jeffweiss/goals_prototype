b8ae42b978629040e760b185d77c20a556ad9afc
Jeff Weiss <jeff.weiss@puppet.com>
2016-05-05 13:01:35 -0700

    experiment with included data

--- web/views/goal_view.ex
+++ web/views/goal_view.ex
@@ -2,8 +2,13 @@ defmodule Sips.GoalView do
   use Sips.Web, :view
   use JaSerializer.PhoenixView
 
+  location :goal_url
   attributes [:name]
   has_one :owner, link: :user_link
+
+  def goal_url(goal, conn) do
+    goal_url(conn, :show, goal)
+  end
 
   def user_link(goal, conn) do
     user_url(conn, :show, goal.owner_id)
--- web/views/list_view.ex
+++ web/views/list_view.ex
@@ -1,10 +1,21 @@
 defmodule Sips.ListView do
   use Sips.Web, :view
   use JaSerializer.PhoenixView
+  require Ecto.Query
 
+  location :list_url
   attributes [:name]
   has_one :owner, link: :user_link
-  has_many :goals, link: :goal_link
+  has_many :goals, serializer: Sips.GoalView, link: :goal_link
+
+  def goals(list, _conn) do
+    Ecto.Query.from(l in Sips.List, join: g in assoc(l, :goals), where: l.id == ^list.id, select: g)
+    |> Sips.Repo.all
+  end
+
+  def list_url(list, conn) do
+    list_url(conn, :show, list)
+  end
 
   def user_link(list, conn) do
     user_url(conn, :show, list.owner_id)
